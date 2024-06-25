113671c018ef1768566e59a3451e285e20b4f2fc
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-06-25 00:28:43 -0300

    commit 005: añadiendo apartado admin

--- src/init.py
+++ src/init.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
+# Manejador de error 404
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('pages/404.html')
 
+# Rutas del sitio web
 @app.route('/')
 def index():
     return render_template('pages/index.html')
@@ -18,6 +20,7 @@ def habitaciones():
 def nosotros():
     return render_template('pages/nosotros.html')
 
+# Rutas del apartado administrador
 @app.route('/admin')
 def admin_index():
     return render_template('admin/index.html')
@@ -26,5 +29,10 @@ def admin_index():
 def admin_login():
     return render_template('admin/login.html')
 
+@app.route('/admin/habitaciones')
+def admin_habitaciones():
+    return render_template('admin/habitaciones.html')
+
+# Corriendo la aplicación
 if __name__ == '__main__':
     app.run(debug=True)
